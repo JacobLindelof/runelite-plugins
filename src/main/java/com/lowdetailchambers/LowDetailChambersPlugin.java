d76569781d86bad0558eae245dd5f812473853f2
bepzi <bepzi@bepzi.com>
2024-03-09 13:35:02 -0800

    Fix low detail not being enabled at the start of the raid

--- src/main/java/com/lowdetailchambers/LowDetailChambersPlugin.java
+++ src/main/java/com/lowdetailchambers/LowDetailChambersPlugin.java
@@ -73,7 +73,7 @@ public class LowDetailChambersPlugin extends Plugin
 	@Subscribe
 	public void onVarbitChanged(VarbitChanged event)
 	{
-		if (event.getVarbitId() == Varbits.IN_RAID || event.getVarpId() == VarPlayer.IN_RAID_PARTY)
+		if (event.getVarbitId() == Varbits.IN_RAID || event.getVarpId() == VarPlayer.IN_RAID_PARTY || event.getVarbitId() == Varbits.RAID_STATE)
 		{
 			if (!lowDetailDisabled())
 			{
@@ -91,9 +91,31 @@ public class LowDetailChambersPlugin extends Plugin
 
 	private boolean insideChambersOfXeric()
 	{
+		if (client.getVarbitValue(Varbits.IN_RAID) != 1)
+		{
+			// Not inside the lobby or the raid levels.
+			return false;
+		}
+
 		int raidPartyID = client.getVarpValue(VarPlayer.IN_RAID_PARTY);
-		boolean inRaidChambers = client.getVarbitValue(Varbits.IN_RAID) == 1;
-		return raidPartyID != -1 && inRaidChambers;
+		if (raidPartyID == -1)
+		{
+			// Raid party ID is -1 when:
+			// 1. We're not in a raid party at all (e.g. outside the raid)
+			// 2. We were in a party but we're currently reloading the raid from the inside stairs
+			// 3. We were in a party but then we started the raid
+			//
+			// Only #3 is a valid reason to enable low detail mode. The other two cases should NOT result
+			// in toggling low detail.
+
+			// The plugin crashes if we check RAID_STATE while not inside Chambers, so only check
+			// RAID_STATE here now that we know it's safe.
+			int raidState = client.getVarbitValue(Varbits.RAID_STATE);
+			return raidState != 0 && raidState != 5;
+		}
+
+		// We're in the lobby, we haven't started the raid, and we're not currently reloading the raid.
+		return true;
 	}
 
 	private boolean lowDetailDisabled()
